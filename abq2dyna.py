fead6ba8cf8eaf53a9770d9bcd236de9fca7c4fc
Ryoooful <36652765+Ryoooful@users.noreply.github.com>
2020-08-21 09:01:42 +0900

    Update abq2dyna.py

--- abq2dyna.py
+++ abq2dyna.py
@@ -11,24 +11,29 @@ with open(path) as f:
     Read_Node = False
     Read_Surface = False
     #Node変数
-    nid = []
-    nx  = []
-    ny  = []
-    nz  = []
+    Node_ID = []
+    Node_X  = []
+    Node_Y  = []
+    Node_Z  = []
 
     #Element変数
-    el_id      = []
-    el_types   = []
-    el_counts  = []
-    el_nodes   = []
+    Element_ID      = []
+    Element_Types   = []
+    Element_Counts  = []
+    Element_Nodes   = []
+
+    ##サーフェス変数
+    Surface_Names = []
+    Surface_Types = []
+    Surface_Elsets = []
 
     ##節点集合(Nset)と要素集合(Elset)の変数
-    st_types          = []
-    st_names          = []
-    st_internals      = []
-    st_generates      = []
-    st_instance_names = []
-    st_nodes          = []
+    Set_Types          = []
+    Set_Names          = []
+    Set_Internals      = []
+    Set_Generates      = []
+    Set_Instance_Names = []
+    Set_Nodes          = []
 
     for line in lines:
     ##コメントは飛ばす
@@ -46,7 +51,7 @@ with open(path) as f:
                 Read_Element = False
             elif Read_Set:
                 Read_Set = False
-                st_nodes += [nodes]
+                Set_Nodes += [Temp_Nodes]
 
     ##KEY値の取込みフラグを立てる
         ##節点の取込みを開始する。
@@ -57,23 +62,30 @@ with open(path) as f:
         ##要素の取込みを開始する。
             elif '*Element' in line:
                 Read_Element = True
-                Element_Type = spdata[1].split("=")[1] 
+                Temp_Type = spdata[1].split("=")[1] 
 
         ##サーフェスの取込みを開始する。
             elif '*Surface' in line:
                 Read_Surface = True
+                Temp_Type = ""
+                Temp_Name = ""
+                for sp in spdata:                    
+                    if "type=" in sp:
+                        Temp_Type = sp.split("=")[1]
+                    elif "name=" in sp:
+                        Temp_Name = sp.split("=")[1]
 
         ##節点集合(Nset)と要素集合(Elset)の取込みを開始する。
             elif '*Nset' in line or '*Elset' in line:
                 Read_Set = True                
                 internal = False
                 generate = False
-                st_name = ""
+                Temp_Name = ""
                 instance_name = ""
 
                 for sp in spdata:                    
                     if "nset=" in sp or "elset=" in sp:
-                        st_name = sp.split("=")[1]
+                        Temp_Name = sp.split("=")[1]
                     elif "instance=" in sp:
                         instance_name = sp.split("=")[1]
                     elif sp.strip() =="internal":
@@ -81,46 +93,46 @@ with open(path) as f:
                     elif sp.strip() =="generate":
                         generate = True
                 
-                st_types += [spdata[0].replace("*","")]
-                st_names += [st_name]
-                st_internals += [internal]
-                st_generates += [generate]
-                st_instance_names += [instance_name]
-                nodes = []
+                Set_Types += [spdata[0].replace("*","")]
+                Set_Names += [Temp_Name]
+                Set_Internals += [internal]
+                Set_Generates += [generate]
+                Set_Instance_Names += [instance_name]
+                Temp_Nodes = []
 
     ##各データの取り込みを行う。
         else:
             ##nodeの取込みを行う。
             if Read_Node:
-                nid += [int(spdata[0])]
-                nx  += [float(spdata[1])]
-                ny  += [float(spdata[2])]
-                nz  += [float(spdata[3])]
+                Node_ID += [int(spdata[0])]
+                Node_X  += [float(spdata[1])]
+                Node_Y  += [float(spdata[2])]
+                Node_Z  += [float(spdata[3])]
 
             ##Elementの取込みを行う。
             if Read_Element:
-                ElementNodes = []
+                Temp_Nodes = []
                 for st in spdata[1:]:
-                    ElementNodes.append(int(st.strip()))
-                el_id     += [int(spdata[0])]
-                el_types  += [Element_Type]
-                el_counts += [int(len(spdata)) - 1]
-                el_nodes  += [ElementNodes]
-                
-            if Read_Surface:
+                    Temp_Nodes.append(int(st.strip()))
+                Element_ID     += [int(spdata[0])]
+                Element_Types  += [Temp_Type]
+                Element_Counts += [int(len(spdata)) - 1]
+                Element_Nodes  += [Temp_Nodes]
 
+            #if Read_Surface:
+                
 
 
             ##節点集合(Nset)と要素集合(Elset)の取込みを行う。
             if Read_Set:
                 if generate:
                     for st in range(int(spdata[0]), int(spdata[1]), int(spdata[2])):
-                        nodes.append(st)
+                        Temp_Nodes.append(st)
                 else:
                     for st in spdata:       
                         if st.strip() != "":
-                            nodes.append(st.strip())
+                            Temp_Nodes.append(st.strip())
 
-NodesFrame   = pd.DataFrame(index=nid, data={'x':nx, 'y':ny, 'z':nz}, columns=['x', 'y', 'z'])
-ElementFrame = pd.DataFrame(index=el_id, data={'type':el_types, 'count':el_counts, 'nodes':el_nodes}, columns=['type', 'count', 'nodes'])
-SetFrame = pd.DataFrame(data={'name':st_names, 'type':st_types, 'instance_name':st_instance_names, 'internal':st_internals, 'generate':st_generates, 'nodes':st_nodes}, columns=['name','type', 'instance_name', 'internal', 'generate', 'nodes'])
+NodesFrame   = pd.DataFrame(index=Node_ID, data={'x':Node_X, 'y':Node_Y, 'z':Node_Z}, columns=['x', 'y', 'z'])
+ElementFrame = pd.DataFrame(index=Element_ID, data={'type':Element_Types, 'count':Element_Counts, 'nodes':Element_Nodes}, columns=['type', 'count', 'nodes'])
+SetFrame = pd.DataFrame(data={'name':Set_Names, 'type':Set_Types, 'instance_name':Set_Instance_Names, 'internal':Set_Internals, 'generate':Set_Generates, 'nodes':Set_Nodes}, columns=['name','type', 'instance_name', 'internal', 'generate', 'nodes'])
