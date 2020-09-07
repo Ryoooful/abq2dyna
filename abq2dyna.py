cd5b91a54ecc6f7cf9cbbe38c75cf81aa4d79487
Ryoooful <36652765+Ryoooful@users.noreply.github.com>
2020-09-07 23:37:04 +0900

    Update abq2dyna.py

--- abq2dyna.py
+++ abq2dyna.py
@@ -57,63 +57,63 @@ for line in lines:
     ##Save Keyword
     if "*" == spdata[0][:1]:
         keyword = spdata[0]
-        if "*Solid Section"   ==      keyword:
-            abaqus["t_solid_id"]["solid_id"]             += [len(abaqus["t_solid_id"]["solid_id"])]
-            abaqus["t_solid_id"]["elset_name"]           += [get_name("elset", spdata)]
-            abaqus["t_solid_id"]["material_name"]        += [get_name("material", spdata)]
-        elif "*Coupling"            ==      keyword:
-            abaqus["t_constraint_name"]["constraint_id"]         += [len(abaqus["t_constraint_name"]["constraint_id"])]
-            abaqus["t_constraint_name"]["constraint_name"]       += [get_name("constraint name", spdata)]
-            abaqus["t_constraint_name"]["nset_name"]             += [get_name("ref node", spdata)]
-            abaqus["t_constraint_name"]["surface_name"]          += [get_name("surface", spdata)]
-        elif  "*Element"          ==      keyword:
-            element_type        = get_name("type", spdata)
-        elif "*Elset"           ==      keyword:
-            elset_name          = get_name("elset", spdata)
-            elset_generate      = get_bool("generate", spdata, False)
-            abaqus["t_elset_name"]["elset_name"]       += [elset_name]
-            abaqus["t_elset_name"]["instance_name"]    += [get_name("instance", spdata)]
-            abaqus["t_elset_name"]["generate"]         += [elset_generate]
-            abaqus["t_elset_name"]["internal"]         += [get_bool("internal", spdata, False)]
-        elif "*Nset"            ==      keyword:
-            nset_name           = get_name("nset", spdata)
-            nset_generate       = get_bool("generate", spdata, False)
-            abaqus["t_nset_name"]["nset_name"]        += [nset_name]
-            abaqus["t_nset_name"]["instance_name"]    += [get_name("instance", spdata)]
-            abaqus["t_nset_name"]["generate"]         += [nset_generate]
-            abaqus["t_nset_name"]["internal"]         += [get_bool("internal", spdata, False)]
-        elif "*Tie"            ==      keyword:
-            abaqus["t_tie_name"]["tie_id"]           += [len(abaqus["t_tie_name"]["tie_id"])]
-            abaqus["t_tie_name"]["tie_name"]         += [get_name("name", spdata)]
-            abaqus["t_tie_name"]["adjust"]           += [get_bool("adjust", spdata, True)]
-        elif "*Material"            ==      keyword:
-            material_name       = get_name("name", spdata)
-            abaqus["t_material_name"]["material_id"]             += [len(abaqus["t_material_name"]["material_id"])]
-            abaqus["t_material_name"]["material_name"]           += [material_name]
-            abaqus["t_material_name"]["hyperelastic"]            += []
+        if "*Solid Section" == keyword:
+            abaqus["t_solid_id"]["solid_id"]                += [len(abaqus["t_solid_id"]["solid_id"])]
+            abaqus["t_solid_id"]["elset_name"]              += [get_name("elset", spdata)]
+            abaqus["t_solid_id"]["material_name"]           += [get_name("material", spdata)]
+        elif "*Coupling" == keyword:
+            abaqus["t_constraint_name"]["constraint_id"]    += [len(abaqus["t_constraint_name"]["constraint_id"])]
+            abaqus["t_constraint_name"]["constraint_name"]  += [get_name("constraint name", spdata)]
+            abaqus["t_constraint_name"]["nset_name"]        += [get_name("ref node", spdata)]
+            abaqus["t_constraint_name"]["surface_name"]     += [get_name("surface", spdata)]
+        elif "*Element" == keyword:
+            element_type = get_name("type", spdata)
+        elif "*Elset" == keyword:
+            elset_name = get_name("elset", spdata)
+            elset_generate = get_bool("generate", spdata, False)
+            abaqus["t_elset_name"]["elset_name"]            += [elset_name]
+            abaqus["t_elset_name"]["instance_name"]         += [get_name("instance", spdata)]
+            abaqus["t_elset_name"]["generate"]              += [elset_generate]
+            abaqus["t_elset_name"]["internal"]              += [get_bool("internal", spdata, False)]
+        elif "*Nset" == keyword:
+            nset_name = get_name("nset", spdata)
+            nset_generate = get_bool("generate", spdata, False)
+            abaqus["t_nset_name"]["nset_name"]              += [nset_name]
+            abaqus["t_nset_name"]["instance_name"]          += [get_name("instance", spdata)]
+            abaqus["t_nset_name"]["generate"]               += [nset_generate]
+            abaqus["t_nset_name"]["internal"]               += [get_bool("internal", spdata, False)]
+        elif "*Tie" == keyword:
+            abaqus["t_tie_name"]["tie_id"]                  += [len(abaqus["t_tie_name"]["tie_id"])]
+            abaqus["t_tie_name"]["tie_name"]                += [get_name("name", spdata)]
+            abaqus["t_tie_name"]["adjust"]                  += [get_bool("adjust", spdata, True)]
+        elif "*Material" == keyword:
+            material_name = get_name("name", spdata)
+            abaqus["t_material_name"]["material_id"]        += [len(abaqus["t_material_name"]["material_id"])]
+            abaqus["t_material_name"]["material_name"]      += [material_name]
+            abaqus["t_material_name"]["hyperelastic"]       += []
             for col in [st for st in abaqus["t_material_name"].keys()][2:]:
                 abaqus["t_material_name"][col] += [0]
-        elif "*Step"            ==      keyword: 
-            step_name           = get_name("name", spdata)
-            abaqus["t_step_name"]["step_id"]             += [len(abaqus["t_step_name"]["material_id"])]
-            abaqus["t_step_name"]["step_name"]           += [step_name]
-        elif "*End Step"            ==      keyword: 
+        elif "*Step" == keyword: 
+            step_name = get_name("name", spdata)
+            abaqus["t_step_name"]["step_id"]                += [len(abaqus["t_step_name"]["step_id"])]
+            abaqus["t_step_name"]["step_name"]              += [step_name]
+        elif "*End Step" == keyword: 
             step_name = ""
 
         continue
 
     ##Append Data
     if   keyword == "*Node":
-        abaqus["t_node_id"]["node_id"]               += [int(spdata[0])]
-        abaqus["t_node_id"]["x"]                     += [float(spdata[1]) * 1000]
-        abaqus["t_node_id"]["y"]                     += [float(spdata[2]) * 1000]
-        abaqus["t_node_id"]["z"]                     += [float(spdata[3]) * 1000]
+        abaqus["t_node_id"]["node_id"]                   += [int(spdata[0])]
+        abaqus["t_node_id"]["x"]                         += [float(spdata[1]) * 1000]
+        abaqus["t_node_id"]["y"]                         += [float(spdata[2]) * 1000]
+        abaqus["t_node_id"]["z"]                         += [float(spdata[3]) * 1000]
     elif keyword == "*Element":
-        abaqus["t_element_id"]["element_id"]         += [int(spdata[0])]
-        abaqus["t_element_id"]["element_type"]       += [element_type]
-        abaqus["t_element_id"]["node_ids"]           += [int(st) for st in spdata[1:] if st.isdecimal()]
-        abaqus["t_element_component"]["element_id"]  += [int(spdata[0])  for st in spdata[1:]]
-        abaqus["t_element_component"]["node_id"]     += [int(st) for st in spdata[1:]]
+        abaqus["t_element_id"]["element_id"]             += [int(spdata[0])]
+        abaqus["t_element_id"]["element_type"]           += [element_type]
+        abaqus["t_element_id"]["node_ids"]               += [[int(st) for st in spdata[1:] if st.isdecimal()]]
+        abaqus["t_element_component"]["element_id"]      += [int(spdata[0])  for st in spdata[1:]]
+        abaqus["t_element_component"]["node_id"]         += [int(st) for st in spdata[1:]]
     elif keyword == "*Elset":        
         if elset_generate:
             abaqus["t_elset_component"]["elset_name"]    += [elset_name  for st in range(int(spdata[0]), int(spdata[1]) + int(spdata[2]), int(spdata[2]))]
@@ -133,7 +133,8 @@ for line in lines:
                 else:
                     tmp = create_table(abaqus["t_nset_component"])
                     for index, row in tmp[tmp["nset_name"] == nset_name].reset_index().iterrows():
-                        abaqus["t_nset_component"]["node_id"]    +=int(row.node_id)
+                        abaqus["t_nset_component"]["nset_name"]  += [nset_name]
+                        abaqus["t_nset_component"]["node_id"]    += [int(row.node_id)]
                     del tmp
     elif keyword == "*Tie":  
         abaqus["t_tie_name"]["slave_surface"]          += [spdata[0]]
@@ -151,6 +152,8 @@ for line in lines:
         abaqus["t_material_name"]["hyperelastic"][len(abaqus["t_material_name"]["hyperelastic"]) - 1]     = [float(st) for st in spdata[:6]]
         keyword = ""
 
+for st in abaqus.keys():
+    abaqus[st] = create_table(abaqus[st])
 
 #Convert .k 
 def get_node_on_surface(element_type, identification, node_ids):
@@ -227,4 +230,7 @@ lsdyna = {
     "t_vid":            {}
         }
 
+abaqus["q_solid_component"] = pd.merge(abaqus["t_solid_id"], abaqus["t_elset_component"], on='elset_name', how='left')
+abaqus["q_part_component"] = pd.merge(abaqus["q_solid_component"], abaqus["t_element_id"], on='element_id', how='left')
 
+print(abaqus["q_part_component"][['solid_id','elset_name','material_name','element_type']].groupby('solid_id').max().reset_index())
